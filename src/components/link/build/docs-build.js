21c422f8525982ca6a69030db3fdac74787835e3
Allen Taylor <allen.taylor@paviasystems.com>
2018-08-01 16:18:38 -0700

    refactor(cedar link): update api archiving process for cedar link

--- src/components/link/build/docs-build.js
+++ src/components/link/build/docs-build.js
@@ -2,6 +2,6 @@ const docsBuild = require('../../../../build/component-docs-build');
 const path = require('path');
 const pkgInfo = require('../package.json');
 
-const vueFilePath = path.resolve(__dirname, '..', `${process.env.npm_package_config_vue_file}.vue`);
+const LinkPath = path.resolve(__dirname, '..', 'CdrLink.vue');
 
-docsBuild(vueFilePath, pkgInfo);
+docsBuild(LinkPath, pkgInfo);
