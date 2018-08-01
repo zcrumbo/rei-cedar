036351c5b075b70933e7dbebd9982f5a3ef0d64f
Allen Taylor <allen.taylor@paviasystems.com>
2018-08-01 16:44:14 -0700

    refactor(cedar rating): update api archiving process for cedar rating
    affects: @rei/cdr-rating

--- src/components/rating/build/docs-build.js
+++ src/components/rating/build/docs-build.js
@@ -2,6 +2,6 @@ const docsBuild = require('../../../../build/component-docs-build');
 const path = require('path');
 const info = require('../package.json');
 
-const vueFilePath = path.resolve(__dirname, '..', `${process.env.npm_package_config_vue_file}.vue`);
+const RatingPath = path.resolve(__dirname, '..', 'CdrRating.vue');
 
-docsBuild(vueFilePath, info);
+docsBuild(RatingPath, info);
